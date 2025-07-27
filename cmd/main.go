5ba44e634f1413daf238d85f588bce7affcb0732
Mostafa Shirvani <MostafaShirvani1377@outlook.com>
2025-07-27 18:19:47 +0200

    Implement basic Bubble Tea functionality to test

--- cmd/main.go
+++ cmd/main.go
@@ -16,12 +16,29 @@ func (m model) Init() tea.Cmd {
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	return nil, nil
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		if msg.String() == "q" {
+
+			return m, tea.Quit
+		}
+
+	}
+	return m, nil
 }
 func (m model) View() string {
-	return ""
+	return "Welcome to Go Server Tools!\n\nPress 'q' to quit.\n"
 }
 
 func main() {
+	m := model{
+		message: "Starting up...",
+	}
+	p := tea.NewProgram(m)
+	_, err := p.Run()
+	if err != nil {
+		fmt.Printf("Encountred Error : %s", err)
+		os.Exit(1)
 
+	}
 }
