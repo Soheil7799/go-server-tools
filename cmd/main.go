aac17f30d065e58efbcc1a45f0102c0427a91d61
Mostafa Shirvani <MostafaShirvani1377@outlook.com>
2025-07-27 19:51:19 +0200

    Refactor model initialization and update logic for command selection

--- cmd/main.go
+++ cmd/main.go
@@ -8,7 +8,16 @@ import (
 )
 
 type model struct {
-	message string
+	choices  []string
+	cursor   int
+	selected string
+}
+
+func initializeModel() model {
+	return model{
+		choices: []string{"SSH", "rsync", "Exit"},
+		cursor:  0,
+	}
 }
 
 func (m model) Init() tea.Cmd {
@@ -18,22 +27,41 @@ func (m model) Init() tea.Cmd {
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
-		if msg.String() == "q" {
-
+		switch msg.String() {
+		case "down", "j":
+			if m.cursor < len(m.choices)-1 {
+				m.cursor++
+			}
+		case "up", "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+		case "enter", " ":
+			m.selected = m.choices[m.cursor]
 			return m, tea.Quit
-		}
+		case "ctrl+c", "q":
+			return m, tea.Quit
 
+		}
 	}
 	return m, nil
 }
 func (m model) View() string {
-	return "Welcome to Go Server Tools!\n\nPress 'q' to quit.\n"
+	s := "which command do you want to run ?\n\n"
+	for i, c := range m.choices {
+		selectedCursor := "[ ]"
+
+		if m.cursor == i {
+			selectedCursor = "[*]"
+		}
+		s += fmt.Sprintf("%s %s\n", selectedCursor, c)
+	}
+	s += "\n\nYou can quit with ctrl+c or typing \"q\""
+	return s
 }
 
 func main() {
-	m := model{
-		message: "Starting up...",
-	}
+	m := initializeModel()
 	p := tea.NewProgram(m)
 	_, err := p.Run()
 	if err != nil {
