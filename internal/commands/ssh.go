36c89ee6494f5b94b89f1c6ac78502d812a486e9
Mostafa Shirvani <MostafaShirvani1377@outlook.com>
2025-08-03 13:33:13 +0200

    Add ExecuteSSH function to run SSH command

--- internal/commands/ssh.go
+++ internal/commands/ssh.go
@@ -1,1 +1,15 @@
 package commands
+
+import (
+	"os"
+	"os/exec"
+)
+
+func ExecuteSSH(server, key string) error {
+	cmd := exec.Command("ssh", "-i", key, server)
+
+	cmd.Stdin = os.Stdin
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	return cmd.Run()
+}
