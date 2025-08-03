5b15a4d7c030ad341650bf68791afe5e9fbdc901
Mostafa Shirvani <MostafaShirvani1377@outlook.com>
2025-08-03 14:06:38 +0200

    Add Config, Server, and SSHKey types to config package

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,18 @@
 package config
+
+type Server struct {
+	Name        string `yaml:"name"`
+	Host        string `yaml:"host"`
+	Description string `yaml:"description"`
+}
+
+type SSHKey struct {
+	Name        string `yaml:"name"`
+	Path        string `yaml:"path"`
+	Description string `yaml:"description"`
+}
+
+type Config struct {
+	Servers []Server `yaml:"servers"`
+	SSHKeys []SSHKey `yaml:"ssh_keys"`
+}
