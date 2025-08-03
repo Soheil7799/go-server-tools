d0e555f0247c47508737aac91e8c6192fa1812f3
Mostafa Shirvani <MostafaShirvani1377@outlook.com>
2025-08-03 17:45:48 +0200

    Add ExecuteRsync function to run rsync commands

--- internal/commands/rsync.go
+++ internal/commands/rsync.go
@@ -1,1 +1,23 @@
 package commands
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+func ExecuteRsync(server, key, localPath, remotePath string, direction int) error {
+	remoteServerPath := fmt.Sprintf("%s:%s", server, remotePath)
+	remoteServerSSH := fmt.Sprintf("\"ssh -i %s\"", key)
+	var cmd *exec.Cmd
+	if direction == 0 {
+		cmd = exec.Command("rsync", "-az", "-e", remoteServerSSH, localPath, remoteServerPath)
+	} else {
+		cmd = exec.Command("rsync", "-az", "-e", remoteServerSSH, remoteServerPath, localPath)
+	}
+
+	cmd.Stdin = os.Stdin
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	return cmd.Run()
+}
