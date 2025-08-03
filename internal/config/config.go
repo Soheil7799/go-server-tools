268c38608d4490ace250cd44f11aa696545be27a
Mostafa Shirvani <MostafaShirvani1377@outlook.com>
2025-08-03 14:08:22 +0200

    Add YAML config loading to internal/config package

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,5 +1,11 @@
 package config
 
+import (
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
 type Server struct {
 	Name        string `yaml:"name"`
 	Host        string `yaml:"host"`
@@ -15,4 +21,22 @@ type SSHKey struct {
 type Config struct {
 	Servers []Server `yaml:"servers"`
 	SSHKeys []SSHKey `yaml:"ssh_keys"`
+}
+
+func LoadConfig() (*Config, error) {
+	// configPath := "~/.config/go-server-tools/config.yaml"
+	configPath := "configs/example.yaml"
+
+	data, err := os.ReadFile(configPath)
+	if err != nil {
+		return nil, err
+	}
+
+	var config Config
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &config, nil
 }
